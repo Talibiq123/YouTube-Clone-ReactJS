b95e86f5627a7b7972e86cd1d50f477ea0b016e4
Talibiq123 <talibsaeed90@gmail.com>
2023-12-13 19:57:39 +0530

    youtube lite

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const SearchBar = () => {
+    let [search, setSearch] = useState("");
+    let [video, setVideo] = useState("");
+    console.log(video);
+
+    function implementSearch() {
+        axios.get("https://www.googleapis.com/youtube/v3/search", {
+            params: {
+                key: "AIzaSyCNfHIK_Ize8fFFbTnCmJYi6_rwu2whf28",
+                part: "snippet",
+                maxResult: 5,
+                type: "video",
+                q: search
+            }
+        })
+        .then(response => setVideo(response.data.items))
+        .catch((error) =>
+            console.log(error)
+        )
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="Search" 
+            onChange={(e) => setSearch(e.target.value)}
+            />
+            <button onClick={implementSearch}>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar;
